7b3831e90a3a05679c7e81d8c4381443d2423104
khushbuMakro <khushbu@makroagency.com>
2024-11-19 20:12:59 +0530

    makro base product carousel on home page

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,14 @@
+// product carousel section home
+var swiper = new Swiper(".mySwiper", {
+slidesPerView: 2.5,
+spaceBetween: 15,
+freeMode: true,
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+},
+});
+
 // product modal
 
 $(".Click-here").on('click', function() {
